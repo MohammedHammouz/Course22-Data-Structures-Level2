961b3a9ab9910b5b0a1cd92b81683995a951e56c
moha327 <m7amad9595@outlook.com>
2026-02-08 21:26:16 +0200

    HashTable

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("key1", "value1");
+            hashtable.Add(2, "value2");
+            hashtable.Remove(2);
+            hashtable.Add("key3", "value3");
+            hashtable.Add("key4", "value4");
+            Console.WriteLine(hashtable["key3"]);
+            foreach(DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine($"key = {entry.Key},value = {entry.Value}");
+            }
+            //Dictionary<int, int> dic = new Dictionary<int, int> { };
+            Console.ReadLine();
+        }
+    }
+}
