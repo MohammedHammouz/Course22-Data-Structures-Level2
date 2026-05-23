2af6a21bac1c8ac4d0d40a3a11cd6be6f49e27c1
moha327 <m7amad9595@outlook.com>
2026-05-23 21:40:08 +0300

    Add IListCollection

---
+++ IListCollection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IListCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleList<string> myList = new SimpleList<string>();
+            myList.Add("First");
+            myList.Add("Second");
+            myList.Insert(1, "Inserted");
+
+            Console.WriteLine("List after adding and inserting:");
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            myList.RemoveAt(1); // Removes "Inserted"
+            myList[0] = "Updated First"; // Update the first item
+
+
+            Console.WriteLine("\nList after removing and updating:");
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ IListCollection/SimpleList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IListCollection
+{
+    public class SimpleList<T>:IList<T>
+    {
+        private List<T> _items = new List<T>();
+        public T this[int index] { 
+            get => _items[index];
+            set=> _items[index] = value; 
+        }
+        public int Count { get => _items.Count; }
+        public bool IsReadOnly => false;
+        public int IndexOf(T item) => _items.IndexOf(item);
+        public void Insert(int index, T item) => _items.Insert(index, item);
+        public void RemoveAt(int index) => _items.RemoveAt(index);
+        public void Add(T item) => _items.Add(item);
+        public void Clear() => _items.Clear();
+        public bool Contains(T item) => _items.Contains(item);
+        public void CopyTo(T[] array, int index) => _items.CopyTo(array,index);
+        public bool Remove(T item) => _items.Remove(item);
+        IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
+        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    }
+}
